b57fd780ee52a15213acfc65602f499ebe0eed98
Cerlancism <scheduledx@gmail.com>
2020-02-08 17:39:03 +0800

    apps script v8 runtime

--- src/MKOTH.ts
+++ src/MKOTH.ts
@@ -1,9 +1,9 @@
 //#region Polyfills
-import 'core-js/features/array/from'
-import 'core-js/features/array/find'
-import 'core-js/features/array/includes'
-import 'core-js/features/string/includes'
-import 'core-js/features/object/values'
+// import 'core-js/features/array/from'
+// import 'core-js/features/array/find'
+// import 'core-js/features/array/includes'
+// import 'core-js/features/string/includes'
+// import 'core-js/features/object/values'
 //#endregion
 
 import './web-app'
@@ -24,5 +24,10 @@ export function doPost(request: RequestPostBase<any>)
 
 export function doTest()
 {
-    test()
+    var testSet = Array.from(Array(5).keys())
+    Logger.log(testSet.find(x => x == 1))
+    Logger.log(testSet.includes(1))
+    Logger.log("test".includes("t"))
+    Logger.log(Object.values({test: "lol"}))
+    test();
 }
